2959fec4f8a144445d29e20c4918a5d0c6a26c14
William Ford <wford1.home@gmail.com>
2021-04-21 18:09:03 +0100

    Added in basic footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/footer.css"
+import githubLogo from "../images/Github-logo.png";
+
+const Footer = () => {
+    return(
+        <footer>
+        <p className="creator">
+        Created By Will Ford 
+      </p>
+        
+        </footer>
+    )
+}
+
+export default Footer
